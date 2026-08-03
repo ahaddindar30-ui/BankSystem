a23126dc5768d706d5f1cd48350fe0ed07172148
ahaddindar30-ui <ahaddin30@gmail.com>
2026-08-03 23:15:29 +0330

    add ApplicationRunner

--- src/main/java/com/mysite/BankSystem/ApplicationRunner.java
+++ src/main/java/com/mysite/BankSystem/ApplicationRunner.java
@@ -1,7 +1,18 @@
 package com.mysite.BankSystem;
 
+import com.mysite.BankSystem.view.ConsoleUI;
+
 public class ApplicationRunner {
+
+
     public static void main(String[] args) {
+        try (ConsoleUI consoleUI = new ConsoleUI()) {
+            consoleUI.startMenu();
+
+        }
+
+
 
     }
+
 }
