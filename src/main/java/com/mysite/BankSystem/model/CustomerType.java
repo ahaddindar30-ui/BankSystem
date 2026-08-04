058f18a5a28d15aa77fb24cf483dc7490e6d2bf2
ahaddindar30-ui <ahaddin30@gmail.com>
2026-08-04 17:09:21 +0330

    add refactor: use CustomerType enum with Factory and Chain of Responsibility to simplify ConsoleUI logic

--- src/main/java/com/mysite/BankSystem/model/CustomerType.java
+++ src/main/java/com/mysite/BankSystem/model/CustomerType.java
@@ -1,6 +1,26 @@
 package com.mysite.BankSystem.model;
 
 public enum CustomerType {
-    REAL,
-    LEGAL
+    REAL(1),
+    LEGAL(2);
+
+    private final int value;
+    CustomerType(int value) {
+        this.value = value;
+    }
+
+    public static CustomerType fromValue(int value) {
+        for (CustomerType type : values()) {
+            if (type.value == value) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+
+
+
+
 }
+
