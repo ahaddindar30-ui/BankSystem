740474ca6d3a0715d7a61f4bf5cb320d2599da90
ahaddindar30-ui <ahaddin30@gmail.com>
2026-08-04 17:14:33 +0330

    add refactor: update CRUD operations to use ID and add deleted view

--- src/main/java/com/mysite/BankSystem/view/ConsoleUI.java
+++ src/main/java/com/mysite/BankSystem/view/ConsoleUI.java
@@ -1,25 +1,28 @@
 package com.mysite.BankSystem.view;
 
 import com.mysite.BankSystem.model.Customer;
-import com.mysite.BankSystem.model.LegalCustomer;
-import com.mysite.BankSystem.model.RealCustomer;
+import com.mysite.BankSystem.model.CustomerType;
 import com.mysite.BankSystem.service.CustomerService;
+import com.mysite.BankSystem.util.ScannerWrapper;
+import com.mysite.BankSystem.view.component.AbstractCustomerUI;
 
 import java.util.List;
-import java.util.Scanner;
+import java.util.function.Function;
 
 public class ConsoleUI implements AutoCloseable {
-    private final CustomerService customerService = new CustomerService();
-    private final Scanner scanner = new Scanner(System.in);
+    private final CustomerService customerService;
+    private final ScannerWrapper scannerWrapper;
 
+    public ConsoleUI() {
+        this.customerService = CustomerService.getInstance();
+        this.scannerWrapper = ScannerWrapper.getInstance();
+    }
 
     public void startMenu() {
         int choice;
         do {
             printMenu();
-            System.out.print("Enter Choice: ");
-            choice = scanner.nextInt();
-            scanner.nextLine();
+            choice = scannerWrapper.getUserInput("Enter Choice: ",Integer::valueOf);
             switch (choice) {
                 case 0:
                     System.out.println("Exit");
@@ -37,16 +40,19 @@ public class ConsoleUI implements AutoCloseable {
                     searchAndPrintCustomerByFamily();
                     break;
                 case 5:
-                    searchAndEditCustomerByName();
+                    editCustomerById();
                     break;
                 case 6:
-                    searchAndDeleteCustomerByName();
+                    deletedCustomerById();
+                    break;
+                case 7:
+                    printAllDeletedCustomers();
                     break;
                 default:
                     System.out.println("Invalid Choice");
             }
         } while (choice != 0);
-        scanner.close();
+        scannerWrapper.close();
 
 
     }
@@ -57,55 +63,44 @@ public class ConsoleUI implements AutoCloseable {
         System.out.println("0.Exit");
         System.out.println("1.Add Customer");
         System.out.println("2.Print All Customers");
-        System.out.println("3.Search and print Customers By Name");
-        System.out.println("4.Search and print Customers By family");
-        System.out.println("5.Search and edit Customers By name");
-        System.out.println("6.Search and deleted Customers By name");
+        System.out.println("3.Search and Customers By Name");
+        System.out.println("4.Search and Customers By family");
+        System.out.println("5.Edit Customers By id");
+        System.out.println("6.Delete Customers By id");
+        System.out.println("7.print all deleted customers");
         System.out.println();
     }
 
-
-    private String getUserInput(String message) {
-        System.out.println(message);
-        return scanner.nextLine();
-
-    }
 
     public void addCustomers() {
         System.out.println("Customer type:");
         System.out.println("1.REAL");
         System.out.println("2.LEGAL");
         System.out.println();
-        System.out.print("Enter your choice: ");
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-        if (choice == 1) {
-            String name = getUserInput("Enter your name: ");
-            String number = getUserInput("Enter your number: ");
-            String email = getUserInput("Enter your email: ");
-            String family = getUserInput("Enter your family: ");
-            String nationalCode = getUserInput("Enter your nationalCode: ");
-            RealCustomer realCustomer = new RealCustomer(name, number, email);
-            realCustomer.setFamily(family);
-            realCustomer.setNationalCode(nationalCode);
-            customerService.addCustomers(realCustomer);
-        } else if (choice == 2) {
-            String name = getUserInput("Enter your name: ");
-            String number = getUserInput("Enter your number: ");
-            String email = getUserInput("Enter your email: ");
-            String fax = getUserInput("Enter your fax number: ");
-            String companyRegistration = getUserInput("Enter your company Registration: ");
-            LegalCustomer legalCustomer = new LegalCustomer(name, number, email);
-            legalCustomer.setFaxNumber(fax);
-            legalCustomer.setCompanyRegistration(companyRegistration);
-            customerService.addCustomers(legalCustomer);
-        }
+        int choice = scannerWrapper.getUserInput("Enter your choice: ",Integer::valueOf);
+
+        Customer customer = AbstractCustomerUI.fromCustomerUI(
+                        CustomerType.fromValue(choice))
+                .generateCustomerUI();
+        customerService.addCustomers(customer);
 
 
     }
 
+    public void printAllDeletedCustomers() {
+        List<Customer> allCustomer = customerService.getDeletedCustomers();
+        if (allCustomer.isEmpty()) {
+            System.out.println("There is no  deleted customers.");
+        } else {
+            System.out.println("All deleted Customer: ");
+            for (Customer customer : allCustomer) {
+                System.out.println(customer);
+            }
+        }
+    }
+
     public void printAllCustomers() {
-        List<Customer> allCustomer = customerService.getAllCustomers();
+        List<Customer> allCustomer = customerService.getActiveCustomers();
         if (allCustomer.isEmpty()) {
             System.out.println("There is no customer to add.");
         } else {
@@ -117,54 +112,41 @@ public class ConsoleUI implements AutoCloseable {
     }
 
     private void searchAndPrintCustomerByName() {
-        String name = getUserInput("Enter your the name: ");
-        List<Customer> customers = customerService.searchCustomersByName(name);
+        String name = scannerWrapper.getUserInput("Enter your the name: ", Function.identity());
+        List<Customer> customers = customerService.printCustomersByName(name);
         customers.forEach(System.out::println);
 
     }
 
     private void searchAndPrintCustomerByFamily() {
-        String family = getUserInput("Enter your the family: ");
+        String family = scannerWrapper.getUserInput("Enter your the family: ", Function.identity());
         List<Customer> customers = customerService.printCustomerByFamily(family);
         customers.forEach(System.out::println);
 
     }
 
-    public void searchAndEditCustomerByName() {
-        String name = getUserInput("Enter your the name: ");
-        List<Customer> customers = customerService.searchCustomersByName(name);
-        for (Customer customer : customers) {
-            if (customer.getName().equals(name)) {
-                String theName = getUserInput("Enter your the name: ");
-                customer.setName(theName);
-                String number = getUserInput("Enter your the number: ");
-                customer.setNumber(number);
-                String email = getUserInput("Enter your the email: ");
-                customer.setEmail(email);
-                if (customer instanceof RealCustomer realCustomer) {
-                    String family = getUserInput("Enter your the family: ");
-                    realCustomer.setFamily(family);
-                    String nationalCode = getUserInput("Enter your the nationalCode: ");
-                    realCustomer.setNationalCode(nationalCode);
-                } else if (customer instanceof LegalCustomer legalCustomer) {
-                    String fax = getUserInput("Enter your the fax number: ");
-                    legalCustomer.setFaxNumber(fax);
-                    String companyRegistration = getUserInput("Enter your the company Registration: ");
-                    legalCustomer.setCompanyRegistration(companyRegistration);
-                }
-            }
-        }
-
+    public void editCustomerById() {
+        String id = scannerWrapper.getUserInput("Enter your the id: ", Function.identity());
+        Customer customer = customerService.editeCustomerById(Integer.valueOf(id));
+        String name = scannerWrapper.getUserInput("Enter your new name: ", Function.identity());
+        customer.setName(name);
+        String number = scannerWrapper.getUserInput("Enter your new number: ", Function.identity());
+        customer.setNumber(number);
+        String email = scannerWrapper.getUserInput("Enter your new email: ", Function.identity());
+        customer.setEmail(email);
+        AbstractCustomerUI
+                .fromCustomerUI(customer.getType())
+                .editCustomer(customer);
     }
 
-    private void searchAndDeleteCustomerByName() {
-        String name = getUserInput("Enter your the name: ");
-        customerService.searchDeleteCustomerByName(name);
+    private void deletedCustomerById() {
+        String id = scannerWrapper.getUserInput("Enter your customer id: ", Function.identity());
+        customerService.deleteCustomerById(Integer.valueOf(id));
     }
 
 
     @Override
     public void close() {
-        scanner.close();
+        scannerWrapper.close();
     }
 }
