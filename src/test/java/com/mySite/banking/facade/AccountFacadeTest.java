e8b62940e69d3eec1f5c62a2ff493215357173aa
ahaddindar30-ui <ahaddin30@gmail.com>
2026-08-26 03:00:12 +0330

    test: add account service and facade tests

---
+++ src/test/java/com/mySite/banking/facade/AccountFacadeTest.java
@@ -0,0 +1,79 @@
+package com.mySite.banking.facade;
+
+
+import com.mysite.banking.dto.AccountDto;
+import com.mysite.banking.dto.AmountDto;
+import com.mysite.banking.facade.AccountFacade;
+import com.mysite.banking.facade.impl.AccountFacadeImpl;
+import com.mysite.banking.facade.impl.CustomerFacadeImpl;
+import com.mysite.banking.model.Account;
+import com.mysite.banking.model.RealCustomer;
+import com.mysite.banking.service.AccountService;
+import com.mysite.banking.service.CustomerService;
+import com.mysite.banking.service.exception.CustomerNotFindException;
+import com.mysite.banking.service.exception.ValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountFacadeTest {
+    @Mock
+    private CustomerService customerService;
+    @Mock
+    private AccountService accountService;
+
+
+    private AccountFacade accountFacade;
+
+
+    @BeforeEach
+    public void setup() {
+        accountFacade = AccountFacadeImpl.getInstance();
+        CustomerFacadeImpl.getInstance(customerService);
+        AccountFacadeImpl.getInstance(accountService);
+    }
+
+    @Test
+    public void addAccountTest() throws CustomerNotFindException {
+        AccountDto accountDto = new AccountDto();
+        accountDto.setCustomerId(10);
+        accountDto.setBalance(new AmountDto(Currency.getInstance("USD"),BigDecimal.ZERO));
+        when(customerService.getCustomerById(10))
+                .thenThrow(new CustomerNotFindException());
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            accountFacade.addAccounts(accountDto);
+        });
+        assertEquals("Customer id is not valid.", exception.getMessage());
+
+
+    }
+
+    @Test
+    public void addAccount() throws CustomerNotFindException, ValidationException {
+        AccountDto accountDto = new AccountDto();
+        accountDto.setCustomerId(10);
+        accountDto.setBalance(new AmountDto(Currency.getInstance("USD"),BigDecimal.ZERO));
+        when(customerService.getCustomerById(10))
+                .thenReturn(new RealCustomer());
+
+
+            accountFacade.addAccounts(accountDto);
+
+
+        verify(accountService)
+                .addAccounts(any(Account.class));
+
+
+    }
+}
---
+++ src/test/java/com/mySite/banking/service/AccountServiceTest.java
@@ -0,0 +1,290 @@
+package com.mySite.banking.service;
+
+import com.mysite.banking.model.Account;
+import com.mysite.banking.model.Amount;
+import com.mysite.banking.service.AccountService;
+import com.mysite.banking.service.exception.AccountNotFindException;
+import com.mysite.banking.service.exception.ValidationException;
+import com.mysite.banking.service.impl.AccountServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AccountServiceTest {
+
+    private AccountService accountService;
+
+    @BeforeEach
+    public void setup(){
+        accountService = AccountServiceImpl.getInstance();
+    }
+
+
+    @Test
+    public void depositCaseA() throws AccountNotFindException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.ZERO));
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.0)));
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(10.0).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+    @Test
+    public void depositCaseB() throws AccountNotFindException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(20.0)));
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.0)));
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(30.0).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+
+    @Test
+    public void depositCaseC() throws AccountNotFindException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(20.2)));
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.1)));
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(30.3).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+    @Test
+    public void depositCaseD() throws AccountNotFindException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(50.1)));
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(30.2)));
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(80.3).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+    @Test
+    public void depositCaseE() throws AccountNotFindException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(2.1)));
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(3.2)));
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(5.3).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+    @Test
+    public void depositCaseF() throws AccountNotFindException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.ZERO));
+
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        for (int i = 0; i < 10; i++) {
+            accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.0)));
+        }
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(100.0).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+
+    @Test
+    public void depositCaseG() throws AccountNotFindException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.ZERO));
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        Runnable runnable = () -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(100.0)));
+                } catch (AccountNotFindException ignored) {
+
+                }
+            }
+
+        };
+
+        runnable.run();
+
+
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(1000.0).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+    @Test
+    public void depositCaseH() throws AccountNotFindException, InterruptedException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.ZERO));
+
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        Runnable depositTask = () -> {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.0)));
+                } catch (AccountNotFindException ignored) {
+
+                }
+            }
+
+        };
+
+        Thread[] threads = new Thread[1];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(depositTask);
+            threads[i].start();
+        }
+        for (Thread thread : threads) {
+                thread.join();
+
+        }
+
+
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(1000.0).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+
+    @Test
+    public void depositCaseI() throws AccountNotFindException, InterruptedException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.ZERO));
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        Runnable depositTask = () -> {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    accountService.deposit(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.0)));
+                } catch (AccountNotFindException ignored) {
+
+                }
+            }
+
+        };
+
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(depositTask);
+            threads[i].start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+
+        }
+
+
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(10000.0).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+    @Test
+    public void withdrawCaseA() throws AccountNotFindException, ValidationException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.valueOf( 20.3)));
+
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        accountService.withdraw(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf( 10.2)));
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(10.1).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+    @Test
+    public void withdrawCaseB() throws AccountNotFindException, InterruptedException {
+        Account account = new Account();
+        account.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.valueOf( 30000)));
+
+        accountService.addAccounts(account);
+        Integer id = account.getId();
+        Runnable withdrawTask = () -> {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    accountService.withdraw(id,new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.0)));
+                } catch (AccountNotFindException | ValidationException ignored) {
+
+                }
+            }
+
+        };
+
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(withdrawTask);
+            threads[i].start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+
+        }
+
+
+        Account accountById = accountService.getAccountById(id);
+
+        assertEquals(BigDecimal.valueOf(20000.0).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+    @Test
+    public void transferCaseA() throws AccountNotFindException, ValidationException {
+        Account accountA = new Account();
+        accountA.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.1)));
+        accountService.addAccounts(accountA);
+        Integer idA = accountA.getId();
+
+        Account accountB = new Account();
+        accountB.setBalance(new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(20.2)));
+
+        accountService.addAccounts(accountB);
+        Integer idB = accountB.getId();
+
+
+        accountService.transfer(idA,idB, new Amount(Currency.getInstance("USD"),BigDecimal.valueOf(10.1)));
+
+
+        Account accountById = accountService.getAccountById(idB);
+
+        assertEquals(BigDecimal.valueOf(30.3).setScale(2), accountById.getBalance().getValue());
+
+
+    }
+
+
+}
