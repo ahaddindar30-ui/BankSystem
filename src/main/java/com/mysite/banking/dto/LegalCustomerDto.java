e1b57495bd9eb8384a2a5dd3912c65a09f7fcc4a
ahaddindar30-ui <ahaddin30@gmail.com>
2026-09-08 10:42:57 +0330

    fix: finalize project configuration

--- src/main/java/com/mysite/banking/dto/LegalCustomerDto.java
+++ src/main/java/com/mysite/banking/dto/LegalCustomerDto.java
@@ -13,11 +13,11 @@ public class LegalCustomerDto extends CustomerDto {
     private String companyName;
 
     public LegalCustomerDto(Integer id, String name, String number, String email, String password) {
-        super(id, name, number, email, password, CustomerType.REAL);
+        super(id, name, number, email, password, CustomerType.LEGAL);
     }
 
     public LegalCustomerDto() {
-        super(CustomerType.REAL);
+        super(CustomerType.LEGAL);
     }
 
 
